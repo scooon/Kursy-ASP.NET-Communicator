2badee952f0141bdfdc755a57dbb7e22e2162c55
Scooon <scooon@ymail.com>
2021-10-27 20:03:35 +0200

    Initial commit

---
+++ ChatMe/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatMe.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Test()
+        {
+            return "Testowa akcja";
+        }
+    }
+}
---
+++ ChatMe/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatMe.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string displayName { get; set; }
+        public string token { get; set; }
+        public DateTime expire { get; set; }
+        public string about { get; set; }
+        public string password { get; set; }
+        public DateTime lastLogin { get; set; }
+        public DateTime created { get; set; }
+    }
+}
