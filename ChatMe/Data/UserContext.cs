7661d74c60fceb31f1b2281a30f7f8712b4e95e0
Scooon <scooon@ymail.com>
2021-11-03 20:23:13 +0100

    Added database solution

---
+++ ChatMe/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ChatMe.Models;
+
+namespace ChatMe.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ChatMe.Models.User> User { get; set; }
+    }
+}
--- ChatMe/Models/User.cs
+++ ChatMe/Models/User.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace ChatMe.Models
 {
     public class User
@@ -17,5 +16,9 @@ namespace ChatMe.Models
         public string password { get; set; }
         public DateTime lastLogin { get; set; }
         public DateTime created { get; set; }
+
+
     }
+
+
 }
