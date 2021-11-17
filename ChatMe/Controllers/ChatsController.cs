61166782ba0b7324c3d3a7388024844d08b9c851
Scooon <scooon@ymail.com>
2021-11-17 20:14:23 +0100

    17.11.2021 20:14

--- ChatMe/Controllers/ChatsController.cs
+++ ChatMe/Controllers/ChatsController.cs
@@ -54,7 +54,7 @@ namespace ChatMe.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("chatID,isGroupMessage,chatName,chatColor,lastMessageTime")] Chat chat)
+        public async Task<IActionResult> Create([Bind("chatID,isGroupMessage,usersIDs,chatName,chatColor,lastMessageTime")] Chat chat)
         {
             if (ModelState.IsValid)
             {
