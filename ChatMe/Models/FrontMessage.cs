ee56bf02f1f6ad22e44082768b625e5dc294b116
Scooon <scooon@ymail.com>
2022-12-21 20:01:14 +0100

    21.12.2022 20:01

--- ChatMe/Models/FrontMessage.cs
+++ ChatMe/Models/FrontMessage.cs
@@ -16,6 +16,14 @@ namespace ChatMe.Models
             readedBy = _readedBy;
             username = _username;
             isMyMessage = _isMyMessage;
+            if (String.IsNullOrEmpty(_creatorName))
+            {
+                shortcut = getShortcut(_username);
+            }
+            else
+            {
+                shortcut = getShortcut(_creatorName);
+            }
         }
         public int messageID { get; set; }
         public int chatID { get; set; }
@@ -25,5 +33,38 @@ namespace ChatMe.Models
         public DateTime createdTime { get; set; }
         public List<Readed> readedBy { get; set; }
         public bool isMyMessage { get; set; }
+        public string shortcut { get; set; }
+
+        private string getShortcut(string chatName)
+        {
+            string shortcut = "";
+            string[] words = chatName.Split();
+            if (chatName.Contains(' '))
+            {
+                int i = 1;
+                foreach (string word in words)
+                {
+                    if ((i == 1) || (i == words.Length))
+                    {
+                        shortcut += word.Substring(0, 1);
+                    }
+                }
+                return shortcut;
+            }
+            else
+            {
+                if (chatName.Length > 2)
+                {
+                    shortcut = words[0].Substring(0, 1);
+                    shortcut += words[0].Substring(chatName.Length - 1);
+                }
+                else
+                {
+                    shortcut = chatName;
+                }
+                return shortcut;
+            }
+
+        }
     }
 }
